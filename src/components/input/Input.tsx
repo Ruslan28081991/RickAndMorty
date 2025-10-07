78e7e8ffc5cea9c8deda43e2e7c91b880a890340
Ruslan <vich_91@mail.ru>
2025-10-07 12:14:34 +0300

    Рефакторинг кода

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,45 +1,46 @@
-import cn from 'classnames'
+import cn from 'classnames';
 
 import SearchIcon from '../../assets/images/search.svg?react';
 
 import './Input.css';
 
 interface IInput {
-    view: 'filter' | 'form';
-    size?: 'default' | 'small';
-    name?: string; 
-    placeholder?: string;
-    value?: string;
-    onChange?: (value: string) => void
+  view: 'filter' | 'form';
+  size?: 'default' | 'small';
+  placeholder?: string;
+  value?: string | number;
+  onChange?: (value: string) => void;
 }
 
 export const Input = ({
-    view,
-    size,
-    name,
-    placeholder,
-    value,
-    onChange,
+  view = 'form',
+  size,
+  placeholder,
+  value,
+  onChange,
 }: IInput) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        onChange?.(e.target.value)
-    }
-
-    return (
-        <div className={cn('input__wrapper', {
-            input__wrapper_filter: view === 'filter',
-            input__wrapper_form: view === 'form',
-        })}>
-              {view === 'filter' && <SearchIcon />}
-              <input 
-                  className={cn(`input__${size}`)}
-                  type="text"
-                  name={name}
-                  placeholder={placeholder} 
-                  value={value}
-                  onChange={handleChange}
-              /> 
-        </div>
-    )
-}
+  return (
+    <div
+      className={cn('input__wrapper', {
+        input__wrapper_filter: view === 'filter',
+        input__wrapper_form: view === 'form',
+      })}
+    >
+      {view === 'filter' && <SearchIcon />}
+      <input
+        className={cn('input', {
+          input__default: size === 'default',
+          input__small: size === 'small',
+        })}
+        type='text'
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
