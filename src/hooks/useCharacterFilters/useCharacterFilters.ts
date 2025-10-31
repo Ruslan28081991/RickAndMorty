ba98d8f704102faf45f9d68e0c6d20a9723ded45
Ruslan <vich_91@mail.ru>
2025-10-31 22:17:32 +0300

    Работа с фильтрами

---
+++ src/hooks/useCharacterFilters/useCharacterFilters.ts
@@ -0,0 +1,25 @@
+import { useMemo, useState } from "react";
+
+import type { ICharacterFilters, ICharacters } from "@/widgets";
+
+export const useCharacterFilters = (characters: ICharacters[] = []) => {
+    const [filters, setFilters] = useState<ICharacterFilters>({
+        name: '',
+        species: '',
+        gender: '',
+        status: '',
+  });
+
+  const filteredCharacters = useMemo(() => {
+    return characters.filter(character => {
+        const matchesName = character.name.toLowerCase().includes((filters.name || '').toLowerCase());
+        const matchesSpecies = !filters.species || character.species.toLowerCase() === filters.species.toLowerCase();
+        const matchesStatus = !filters.status || character.status.toLowerCase() === filters.status.toLowerCase();
+        const matchesGender = !filters.gender || character.gender.toLowerCase() === filters.gender.toLowerCase();
+
+        return matchesName && matchesSpecies && matchesStatus && matchesGender
+    });
+  }, [characters, filters])
+
+  return { filters, setFilters, filteredCharacters}
+}
